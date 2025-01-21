7b4f79da3b5fd9082bdabfc7fd3dcd505a17ccde
rendy eka febriyanto <rendimana12@gmail.com>
2025-01-21 20:15:07 +0700

    update error return in user handler

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -11,11 +11,11 @@ func AddUser(c *fiber.Ctx) error {
 	user := new(models.User)
 
 	if err := c.BodyParser(user); err != nil {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{ "error": err.Error(), })
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{ "error convert json data ": err.Error(), })
     }
 
 	if err := data.CreateUserDatabase(user); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error add data user to database ": err.Error()})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "User added successfully"})
