71b0b015c8a2001a289182afe45b58be8fbf7223
rendy eka febriyanto <rendimana12@gmail.com>
2025-01-23 19:08:39 +0700

    add cors configuration in main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"rendipilot/simple-golang/database"
 	"rendipilot/simple-golang/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gofiber/fiber/v2"
 	// "github.com/joho/godotenv"
 )
@@ -20,12 +21,21 @@ func main() {
 	// }
 
 	// Initialize database connection ONCE
-	_, err := database.ConnectDatabase() 
-	if err != nil { 
-		log.Fatal("Failed to connect to PostgreSQL:", err) 
-	} 
+	_, err := database.ConnectDatabase()
+	if err != nil {
+		log.Fatal("Failed to connect to PostgreSQL:", err)
+	}
 
-	defer database.GetDB().Close(context.Background()) 
+	defer database.GetDB().Close(context.Background())
+
+	config := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"}, // Allow all methods
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	app.Use(config)
 
 	// Define endpoints
 	app.Get("/", func(c *fiber.Ctx) error {
